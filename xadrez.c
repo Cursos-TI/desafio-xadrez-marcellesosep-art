79dcc699e3fc891a28837f461936faed7e9e0f13
marcellesosep-art <marcelle.sosep@gmail.com>
2025-10-18 18:49:50 +0000

    commit de finalizacao

--- xadrez.c
+++ xadrez.c
@@ -1,6 +1,53 @@
 #include <stdio.h>
-// mover a torre 5 casas para a direita
-for (int i = 0; i < 5 i++) {
-  printf("Direita\n"); 
+int main(){
+
+  // Movimento da torre em For 
+  
+  int posicao da torre = 1;
+  
+  printf("===movimento da torre (for) ===\n");
+  printf("Torre movndo 5 casa para a direita\n");
+
+for (int i = 1; i < = 5; i++)
+{
+  
+  printf("Torre na casa %d\n"); 
 }
-return 0;
+
+// Movimento do bispo em while
+
+int i = 1;
+
+printf("=== Movimento do bispo (while)===\n");
+printf("Bispomovendo 5 casas na diagonal(para cime e direita):\n");
+
+while (passos do bispo < 5); 
+{
+ printf("bispo na posicao:linha %d, coluna %d:\n", linha bispo, coluna bispo);
+}
+ printf("Bispo terminou na linha %d, coluna %d\n\n",linha bispo, coluna bispo);
+
+ 
+ //Movimento da rainha em Do-while
+ 
+ int i = 4;
+ 
+ printf("=== Movimento da Rainha( do-While)===\n");
+ printf("Rainha movendo 8 casas para a esquerda:\n");
+   
+    do {
+        
+      printf("Rainha na posicao:linha %d , coluna %d\n", linha rainha , coluna rainha);
+        
+        while (movimentos < 8 && coluna rainha > 0);
+        
+        printf("Rainha terminou na linha %d, coluna %d\n", linha rainha , coluna rainha);
+        printf ("\n=== fim do jogo de xadrez simplificado!===\n");
+
+        return 0;
+        
+    } 
+ 
+
+
+
